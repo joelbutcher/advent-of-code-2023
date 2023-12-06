b341ed18bd8d772cc3bb304669e32ae093c6c332
Joel Butcher <joel.butcher@gosuperscript.com>
2023-12-06 08:54:22 +0000

    Day 2

---
+++ Day 2/part2.php
@@ -0,0 +1,49 @@
+<?php
+
+$file = file_get_contents('./input.txt');
+
+$limits = [
+    'red' => 12,
+    'green' => 13,
+    'blue' => 14,
+];
+
+// regex to match all numbers suffixed with the color (e.g. 12 blue; 5 red, 3 green)
+$regex = '/(([0-9]+)\s(' . implode('|', array_keys($limits)) . '))/';
+
+echo array_sum(array_map(
+    callback: function (string $line) use ($limits, $regex): int {
+        if (empty($line)) {
+            return 0;
+        }
+
+        // Find the prefix + index for the game
+        preg_match('/(Game )([0-9]+)(:\s)/', $line, $matches);
+
+        // remove the game index from the line
+        $line = str_replace($matches[0], '', $line);
+
+        // turns are separated by a semicolon ';'
+        $turns = explode('; ', $line);
+
+        $max = ['red' => 0, 'green' => 0, 'blue' => 0];
+
+        foreach ($turns as $turn) {
+            preg_match_all($regex, $turn, $matches);
+
+            // if no matches, the turn is invalid, skip it.
+            if (empty($matches[0])) {
+                continue;
+            }
+
+            foreach ($matches[0] as $match) {
+                [$number, $color] = explode(' ', $match);
+
+                $max[$color] = max($max[$color], intval($number));
+            }
+        }
+
+        return array_product($max);
+    },
+    array: explode("\n", $file)
+));
